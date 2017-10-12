e4efe0529865c1386db873705d29375b54cb466d
Paul Angland <pangland101@gmail.com>
2017-10-12 18:10:27 -0400

    condense associatable methods

--- lib_post/associatable.rb
+++ lib_post/associatable.rb
@@ -1,4 +1,4 @@
-require_relative '02_searchable'
+require_relative 'searchable'
 require 'active_support/inflector'
 
 class AssocOptions
@@ -65,6 +65,56 @@ module Associatable
   def assoc_options
     @assoc_options ||= {}
     @assoc_options
+  end
+
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      source_table = source_options.table_name
+
+      source_fkey = source_options.foreign_key
+      source_pkey = source_options.primary_key
+
+      through_pkey = through_options.primary_key
+      through_fkey_val = self.send(through_options.foreign_key).to_s
+
+      blah = DBConnection.execute(<<-SQL,)
+        SELECT #{source_table}.*
+        FROM #{through_table}
+        JOIN #{source_table}
+        ON #{through_table}.#{source_fkey} = #{source_table}.#{source_pkey}
+        WHERE #{through_table}.#{through_pkey} = #{through_fkey_val}
+      SQL
+
+      source_options.model_class.parse_all(blah).first
+    end
+  end
+
+  private
+
+  def through_query(through_name, source_name)
+    through_options = self.class.assoc_options[through_name]
+    source_options = through_options.model_class.assoc_options[source_name]
+
+    through_table = through_options.table_name
+    source_table = source_options.table_name
+
+    source_fkey = source_options.foreign_key
+    source_pkey = source_options.primary_key
+
+    through_pkey = through_options.primary_key
+    through_fkey_val = self.send(through_options.foreign_key).to_s
+
+    DBConnection.execute(<<-SQL,)
+      SELECT #{source_table}.*
+      FROM #{through_table}
+      JOIN #{source_table}
+      ON #{through_table}.#{source_fkey} = #{source_table}.#{source_pkey}
+      WHERE #{through_table}.#{through_pkey} = #{through_fkey_val}
+    SQL
   end
 end
 
